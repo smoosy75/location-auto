df48908d236abb96f4b5f00320beb07f2690df3f
Mustapha Said <mustaphasaid@mustaphas-macbook-air.home>
2023-03-13 23:26:06 +0100

    navbar v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,22 +13,34 @@ function Navbar() {
 
   return (
     <header>
-      <img src="public/images/logo.png" alt="logo" />
-      <nav ref={navRef}>
-        <a href="/#">Accueil</a>
-        <a href="/#">A propos</a>
-        <a href="/#">Modèles</a>
-        <a href="/#">Témoignages</a>
-        <a href="/#">Notre équipe</a>
-        <a href="/#">Contact</a>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <CloseIcon />
-        </button>
-      </nav>
-      <button>sign in</button>
-      <button>Register</button>
+      <div className="navbar_left">
+        <img src="public/images/logo.png" alt="logo" />
+      </div>
+      <div className="navbar_middle">
+        <nav ref={navRef}>
+          <a href="/#">Accueil</a>
+          <a href="/#">A propos</a>
+          <a href="/#">Modèles</a>
+          <a href="/#">Témoignages</a>
+          <a href="/#">Notre équipe</a>
+          <a href="/#">Contact</a>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <CloseIcon
+              sx={{ stroke: "#ffff", strokeWidth: 1 }}
+              style={{ fontSize: 30 }}
+            />
+          </button>
+        </nav>
+      </div>
+      <div className="navbar_right">
+        <button>Mon Espace</button>
+        <button>S'inscrire</button>
+      </div>
       <button className="nav-btn" onClick={showNavbar}>
-        <DensityMediumIcon />
+        <DensityMediumIcon
+          sx={{ stroke: "transparent", strokeWidth: 1 }}
+          style={{ fontSize: 30 }}
+        />
       </button>
     </header>
   );
